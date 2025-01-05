20efbe0f89dbfa63b4a4c7118283ace437a2485e
mohammadhani1 <s12326287@stu.najah.edu>
2025-01-05 05:02:29 +0200

    saved changes before switching to main

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import React  from "react";
+
+import SearchBox from "./Components/SearchBox";
+import Sidebar from "./Components/Sidebar";
+import Content from "./Components/Content";
+
+
+
 
 function App() {
   return (
@@ -5,8 +13,17 @@ function App() {
 
   <img src = "/image.png" classname="logo" alt="logo"/>
 
+   <SearchBox/>
+  <div className="main-container">
+
+
+
+    <Sidebar />
+    <Content />
     </div>
-  );
+    </div>
+   );
 }
 
 export default App;
+
